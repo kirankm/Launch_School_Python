73ce284b00dc517984b24b67d8d947590d0a39a6
kirankm <kiran.k.madan@gmail.com>
2025-04-08 11:11:19 +0530

    Adding validation for Loan Amount

--- PY101/02_Small_Programs/mortgage_calculator.py
+++ PY101/02_Small_Programs/mortgage_calculator.py
@@ -19,10 +19,12 @@
 
 ## Additional Aspects
 # Take Comments Out
-# Prompt separating output 
+# Prompt separating output
 # Internationalization
 # Deal with no interest loans
 # Show all information (input info) along with  output
+# Rounding outputs
+# Multiple Random Messages
 
 ###############################
 # Settings
@@ -37,15 +39,61 @@ def give_introduction():
     prompt("Welcome to the Mortgage Calculator")
     prompt("I will help you figure out your monthly installments")
     prompt("First help me with some information about your loan")
-    
 
+## Getting Loan Amount
+def get_loan_amount():
+    prompt("What is the total Loan Amount")
+    while True:
+        loan_amount_input = input()
+        cleaned_loan_amount = clean_loan_amount(loan_amount_input)
+        if is_valid_loan_amount(cleaned_loan_amount):
+            string_cleaning_warning(cleaned_loan_amount,loan_amount_input)
+            return float(cleaned_loan_amount)
+
+def clean_loan_amount(loan_amount_str):
+    punc_to_remove = [',', '_']
+    for punc in punc_to_remove:
+        loan_amount_str = loan_amount_str.replace(punc, "")
+    return loan_amount_str
+
+def is_valid_loan_amount(loan_amount_str):
+    status = False
+    try:
+        parsed_loan_amount = float(loan_amount_str)
+    except (ValueError, TypeError):
+        prompt("Hmm.. the Loan Amount Doesn't look valid!! Try Again")
+    else:
+        status = check_loan_amount_positive(parsed_loan_amount)
+        if status:
+            status = check_loan_amount_valid(loan_amount_str)
+    return status
+
+def check_loan_amount_valid(loan_amount_str):
+    invalid_inputs = {'nan', 'inf'}
+    if loan_amount_str in invalid_inputs:
+        prompt(f"{loan_amount_str} is not a valid input")
+        return False
+    return True
+
+def check_loan_amount_positive(parsed_loan_amount):
+    if parsed_loan_amount < 0:
+        prompt("Only Positive Values are allowed for Loan Amount!! Try again")
+        return False
+    return True
+
+## Common Functions
+def string_cleaning_warning(cleaned_string, original_string):
+    if cleaned_string != original_string:
+        prompt("WARNING!! THE INPUT STRING WAS CLEANED")
+        prompt(f"{original_string} was converted to {cleaned_string}")
+
+#def clean_input_string(input_string, punc_to_remove = [',', ""])
 
 
 # Program Flow
 give_introduction()
 
-prompt("What is the total Loan Amount")
-loan_amount = input()
+loan_amount = get_loan_amount()
 
 prompt("What is the Annual Interest Rate")
 interest_rate = input()
@@ -60,4 +108,3 @@ denominator = (1 - denominator_part_b)
 monthly_installment = float( loan_amount ) * monthly_interest_rate / denominator
 
 prompt(f"The monthly installment amount is {monthly_installment}")
-

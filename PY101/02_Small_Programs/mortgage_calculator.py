cdaef4e73682aceb7dc1bfe1dfc9ab866f573f07
kirankm <kiran.k.madan@gmail.com>
2025-04-08 11:54:33 +0530

    using status for errors in loan calculator

--- PY101/02_Small_Programs/mortgage_calculator.py
+++ PY101/02_Small_Programs/mortgage_calculator.py
@@ -26,8 +26,19 @@
 # Rounding outputs
 # Multiple Random Messages
 
+#### Is there any way, I can avoid multiple calculation, without validation doing multiple tasks
+##### One option is to have the validation function return 2 values
+
+#### Is it a good idea to move the warning as a part of the status dictionary?
 ###############################
 # Settings
+
+# CONSTANTS
+LOAN_STATUS_DICT = {
+    '0':"IS NOT A VALID LOAN AMOUNT",
+    '1': "IS NOT A VALID INPUT",
+    '2': "Only Positive Values are allowed for Loan Amount!! Try again".upper()
+}
 
 # Functions
 ## General Functions
@@ -46,34 +57,37 @@ def get_loan_amount():
     while True:
         loan_amount_input = input()
         cleaned_loan_amount = clean_input_string(loan_amount_input, [',', ""])
-        if is_valid_loan_amount(cleaned_loan_amount):
+        loan_validation_status = validate_loan_amount(cleaned_loan_amount)
+        if loan_validation_status is True:
             string_cleaning_warning(cleaned_loan_amount,loan_amount_input)
             return float(cleaned_loan_amount)
+        generate_error_prompt(cleaned_loan_amount, loan_validation_status, LOAN_STATUS_DICT)
 
-def is_valid_loan_amount(loan_amount_str):
+def validate_loan_amount(loan_amount_str):
     status = False
     try:
         parsed_loan_amount = float(loan_amount_str)
     except (ValueError, TypeError):
-        prompt("Hmm.. the Loan Amount Doesn't look valid!! Try Again")
+        status = '0'
     else:
         status = check_loan_amount_positive(parsed_loan_amount)
-        if status:
+        if status is True:
             status = check_loan_amount_valid(loan_amount_str)
     return status
 
 def check_loan_amount_valid(loan_amount_str):
     invalid_inputs = {'nan', 'inf'}
-    if loan_amount_str in invalid_inputs:
-        prompt(f"{loan_amount_str} is not a valid input")
-        return False
+    if loan_amount_str.lower() in invalid_inputs:
+        return '1'
     return True
 
 def check_loan_amount_positive(parsed_loan_amount):
     if parsed_loan_amount < 0:
-        prompt("Only Positive Values are allowed for Loan Amount!! Try again")
-        return False
+        return '2'
     return True
+
+
+
 
 ## Common Functions
 def string_cleaning_warning(cleaned_string, original_string):
@@ -85,6 +99,12 @@ def clean_input_string(input_string, punc_to_remove = [',']):
     for punc in punc_to_remove:
         input_string = input_string.replace(punc, "")
     return input_string
+
+def generate_error_prompt(value, status, status_dict):
+    if status == '2':
+        prompt(status_dict[status])
+    else:
+        prompt(f"{value} {status_dict[status]}")
 
 # Program Flow
 give_introduction()

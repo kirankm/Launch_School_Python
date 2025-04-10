aaf613073d0cd806ab2a71da0ab2995100d20c93
kirankm <kirankm@kirankms-MacBook-Air.local>
2025-04-10 09:46:19 +0530

    starting the RPS Code

---
+++ PY101/02_Small_Programs/rps.py
@@ -0,0 +1,20 @@
+## Flow
+# Give Intro Screen
+    # Help, New Game
+# Give Current Game Stat (Round Robin, How Many Done)
+# Get User Input
+# Generate Computer Input
+# Compare User vs Computer and Get Result
+# Declare Result
+    # Show What User did, Computer Did, Result
+# Ask if We should Play Again
+
+
+
+# Common Functions
+def prompt(msg, prefix_space = False):
+    if prefix_space:
+        print("\n")
+    print(f"==> {msg}")
+
+def 

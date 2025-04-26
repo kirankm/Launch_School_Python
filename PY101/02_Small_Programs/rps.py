b82feb65fc79b0c877d70cb1df8839b6bbbb69c6
kirankm <kiran.k.madan@gmail.com>
2025-04-26 19:00:15 +0530

    Finished Adding Personalities

--- PY101/02_Small_Programs/rps.py
+++ PY101/02_Small_Programs/rps.py
@@ -7,8 +7,7 @@ import subprocess
 import random
 import math
 import time
-import pdb
-
+import shutil
 
 # Common Functions
 def prompt(msg, prefix_space = False):
@@ -18,8 +17,19 @@ def prompt(msg, prefix_space = False):
         msg = "\n".join(msg)
     print(f"==> {msg}")
 
-def clear_screen():
+def clear_screen(banner = True):
     subprocess.run('clear', shell=True, check = True)
+    if banner:
+        display_game_banner()
+
+def display_game_banner():
+    terminal_width = shutil.get_terminal_size().columns
+    line1 = "*"*terminal_width
+    line2 = f"*{' '*(terminal_width-2)}*"
+    game_name = 'ROCK PAPER SCISSORS LIZARD SPOCK'.center(terminal_width - 2)
+    line3 = f"*{game_name}*"
+    banner = [line1, line2, line3, line2, line1]
+    print("\n".join(banner))
 
 def get_valid_input(prompt_msg, error_msg, valid_input):
     prompt(prompt_msg, True)
@@ -51,14 +61,27 @@ def check_dict_validation(value_to_check, validation_dict):
         ]
     return in_values
 
+def write_to_json_file(file_path, value):
+    with open(file_path,'w') as f:
+        json.dump(value, f, indent = 2)
+
+def load_json_file(json_file_path):
+    with open(json_file_path,'r') as f:
+        loaded_file = json.load(f)
+    return loaded_file
+
+def get_json_file_path(file_name):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    _, ext = os.path.splitext(file_name)
+    if ext == '':
+        file_name += ".json"
+    json_file_path = os.path.join(script_dir, file_name)
+    return json_file_path
+
 # Pre Load Game Functions
 def load_rps_files(file_name):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    json_file_path = os.path.join(script_dir, file_name)
-
-    with open(json_file_path,'r') as f:
-        json_file = json.load(f)
-    return json_file
+    json_file_path = get_json_file_path(file_name)
+    return load_json_file(json_file_path)
 
 ################ Introduction
 def introduce(menu_options):
@@ -78,21 +101,21 @@ def show_help_screen(move_choices, winning_moves):
 
 def introduce_help_screen():
     clear_screen()
-    prompt(RPS_COMMENTS['help_screen_intro'])
+    prompt(RPS_COMMENTS['help_screen_intro'], prefix_space= True)
 
 def show_shortcut_keys(choices):
     prompt(RPS_COMMENTS["moves_menu"], prefix_space= True)
-    for index, move in enumerate(choices):
+    for move in choices:
         msg = f"Enter {move} for {choices[move]}"
         print(msg)
 
 def show_rules(rules):
     prompt("These are the rules in the Game", prefix_space= True)
-    for index, winning_move in enumerate(rules):
+    for winning_move in rules:
         for losing_move, defeating_action in rules[winning_move].items():
             msg = f"{winning_move} {defeating_action} {losing_move}"
             print(msg)
-    
+
 def show_additional_options():
     prompt(RPS_COMMENTS['additional_options'], prefix_space= True)
 
@@ -103,14 +126,14 @@ def exit_help_screen():
 ################# config screen functions
 def edit_config(settings):
     new_config = settings.copy()
-    while True: 
+    while True:
         introduce_config_screen()
         edit_config_function = get_config_edit_function(settings)
         if edit_config_function is None:
             break
         edit_config_function(new_config)
-    updated_config = remove_redundant_updates(settings, new_config)
-    return updated_config
+    final_config = remove_redundant_updates(settings, new_config)
+    return final_config
 
 def introduce_config_screen():
     clear_screen()
@@ -122,17 +145,18 @@ def get_config_edit_function(settings):
     config_error_msg = RPS_COMMENTS['config_screen_invalid_selection'].copy()
     config_error_msg += config_edit_msg
 
-    user_choice = get_valid_input(config_edit_msg, config_error_msg, 
+    user_choice = get_valid_input(config_edit_msg, config_error_msg,
                                   config_options_dict)
     config_edit_fn_name = config_options_dict[user_choice]
-    config_edit_fn = None if not(config_edit_fn_name) else eval(config_edit_fn_name)
-    return config_edit_fn
+    if config_edit_fn_name:
+        return eval(config_edit_fn_name)
+    return None
 
 def show_current_val(settings_file, setting_display_name, setting_key):
     current_setting_value = settings_file[setting_key]
     msg = f"The current {setting_display_name} is {current_setting_value}"
     prompt(msg)
-    
+
 def get_new_value_from_options(list_of_options, setting_display_name):
     get_value_msg = f"Choose the new value for {setting_display_name}"
     err_msg = f"Invalid value given for {setting_display_name}. Choose from:"
@@ -143,9 +167,9 @@ def get_new_value_from_options(list_of_options, setting_display_name):
     err_msg_full = err_msg + "\n" + choose_msg
 
     valid_user_inputs = [option[0].lower() for option in list_of_options]
-    user_input = get_valid_input(get_value_msg_full, err_msg_full, 
+    user_input = get_valid_input(get_value_msg_full, err_msg_full,
                             valid_user_inputs)
-    new_value = [val for val in list_of_options 
+    new_value = [val for val in list_of_options
                  if val[0].lower() == user_input][0]
     return new_value
 
@@ -169,7 +193,7 @@ def edit_game_mode(new_config):
     list_of_options = new_config['game_mode_options']
     user_chosen_value = get_new_value_from_options(list_of_options,
                                                    "Game Mode")
-    new_config["game_mode"] = user_chosen_value 
+    new_config["game_mode"] = user_chosen_value
 
 def edit_player_name(new_config):
     clear_screen()
@@ -177,8 +201,8 @@ def edit_player_name(new_config):
     prompt("Choose the new value for User Name")
     user_chosen_value = get_new_value_from_user(is_len_gt_0,
                                                 "Player Name")
-    new_config["name_of_player"] = user_chosen_value 
-    
+    new_config["name_of_player"] = user_chosen_value.capitalize()
+
 def is_len_gt_0(string):
     return len(string) > 0
 
@@ -219,8 +243,7 @@ def remove_redundant_updates(settings, new_config):
     }
 
 def update_and_save_config(current_settings, updated_settings):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    json_file_path = os.path.join(script_dir, "rps_config.json")
+    json_file_path = get_json_file_path("rps_config.json")
 
     new_settings = current_settings | updated_settings
     with open(json_file_path,'w') as f:
@@ -228,30 +251,39 @@ def update_and_save_config(current_settings, updated_settings):
 
 ############################# new game functions
 def start_new_game(settings):
-    PLAYER_NAME = settings["name_of_player"].capitalize()
-    new_game_introduction(PLAYER_NAME)
-    history = initialize_game_history()
+    player_name = settings["name_of_player"].capitalize()
+    new_game_introduction(player_name)
+    history = initialize_game_history(settings, player_name)
     while check_game_incomplete(history, settings["no_of_rounds"]):
-        round_winner = play_new_round(settings, history, PLAYER_NAME)
+        round_winner = play_new_round(settings, history, player_name)
         update_history(history, round_winner)
-        continue_next_round = should_continue_next_round(history, 
+        continue_next_round = should_continue_next_round(history,
                                                     settings["no_of_rounds"])
         if continue_next_round == 'q':
             break
-    display_final_result(history, settings["no_of_rounds"], PLAYER_NAME)
+    display_final_result(history, settings["no_of_rounds"], player_name)
+    if not check_game_incomplete(history, settings["no_of_rounds"]):
+        save_game_history(settings, history)
     return should_play_again() == "y"
-     
+
 def new_game_introduction(user_name):
     intro_msg = RPS_COMMENTS['new_game_intro'].replace('user', user_name)
     clear_screen()
     prompt(intro_msg)
 
-def initialize_game_history():
-    return {'user': 0 , 'computer': 0, 'last_match':None}
+def initialize_game_history(settings, player_name):
+    game_history = {'user': 0 , 'computer': 0, 'last_match':None}
+    game_history['game_mode'] = settings['game_mode']
+    game_history['AI_personality'] = settings['AI_personality']
+    game_history['no_of_rounds'] = settings['no_of_rounds']
+    game_history['player_name'] = player_name
+    game_history['moves'] = []
+    return game_history
 
 def check_game_incomplete(curr_history, total_rounds):
     cutoff = math.ceil(total_rounds / 2)
-    return (curr_history['user'] < cutoff) and (curr_history['computer'] < cutoff)
+    return (curr_history['user'] < cutoff) and \
+            (curr_history['computer'] < cutoff)
 
 def update_history(curr_history, winner):
     if winner in curr_history:
@@ -269,6 +301,7 @@ def should_continue_next_round(curr_history, no_of_rounds):
         prompt(msg, prefix_space = True)
         user_input = input()
         return user_input
+    return None
 
 def display_final_result(game_history, no_of_rounds, user_name):
     if check_game_incomplete(game_history, no_of_rounds):
@@ -286,7 +319,7 @@ def display_final_result(game_history, no_of_rounds, user_name):
 def should_play_again():
     input_msg = RPS_COMMENTS["play_tournament_again"]
     error_msg = RPS_COMMENTS["play_tournament_invalid_input"]
-    user_choice = get_valid_input(input_msg, error_msg, 
+    user_choice = get_valid_input(input_msg, error_msg,
                                   validate_should_continue)
     return user_choice[0].lower() ## Make comment based on win or loss
 
@@ -297,11 +330,12 @@ def validate_should_continue(user_input):
 def play_new_round(settings, game_history, user_name):
     new_round_introduction(game_history, settings["no_of_rounds"], user_name)
     user_move = get_user_move(settings['move_choice'], user_name)
-    computer_move = get_computer_move(settings['move_choice'], 
+    computer_move = get_computer_move(settings['move_choice'],
                                       settings['game_mode'], user_move)
     winner = identify_winner(user_move, computer_move, settings['rules'])
     display_round_result(winner, user_move, computer_move, settings['rules'],
                         user_name)
+    update_game_history(game_history, user_move, computer_move)
     return winner
 
 def new_round_introduction(game_history, total_rounds, user_name):
@@ -321,7 +355,7 @@ def display_current_score(history, total_rounds, user_name):
     else:
         leader, lagger = get_leader_lagger(history)
         games_remaining = get_games_remaing(total_rounds, history)
-        
+
         if leader:
             lead = get_lead(history)
             summary = "".join([
@@ -340,11 +374,10 @@ def display_current_score(history, total_rounds, user_name):
 def get_leader_lagger(history):
     if history['user'] > history['computer']:
         return 'user', 'Computer'
-    elif history['user'] < history['computer']:
+    if history['user'] < history['computer']:
         return 'Computer', 'user'
-    else:
-        return None, None
-    
+    return None, None
+
 def get_games_remaing(total_rounds, history):
     game_remaining = total_rounds - history['user'] - history['computer']
     game_remaing_suffix =  'games' if game_remaining > 1 else 'game'
@@ -362,10 +395,11 @@ def get_games_won(history):
 
 def get_user_move(moves, user_name):
     choose_msg  = RPS_COMMENTS['human_move_choose']
-    list_choice_msg = '\n'.join([f'{key} for {val}' for key, val in moves.items()])
+    list_choice_msg = '\n'.join([f'{key} for {val}'
+                                 for key, val in moves.items()])
     comb_msg = choose_msg + '\n' + list_choice_msg + "\n"
 
-    invalid_choice_msg = RPS_COMMENTS['human_move_error'] 
+    invalid_choice_msg = RPS_COMMENTS['human_move_error']
     invalid_choice_msg = invalid_choice_msg + '\n' + list_choice_msg + "\n"
 
     user_choice = get_valid_input(comb_msg, invalid_choice_msg, moves)
@@ -383,12 +417,11 @@ def get_computer_move(choices, game_mode = None, user_move = None):
     return computer_move
 
 def identify_winner(user_move, computer_move, winning_moves):
+    if user_move == computer_move:
+        return None
     if computer_move in winning_moves[user_move]:
         return "user"
-    elif user_move in winning_moves[computer_move]:
-        return "computer"
-    else:
-        return None
+    return "computer"
 
 def display_round_result(winner, user_move, computer_move, winning_moves,
                          user_name):
@@ -407,32 +440,180 @@ def display_round_result(winner, user_move, computer_move, winning_moves,
     prompt(move_msg, prefix_space = True)
     prompt(result_msg)
 
+def update_game_history(history, user_move, computer_move):
+    game_dict = {'user': user_move, 'computer': computer_move}
+    if history['last_match'] == 'tie':
+        history['moves'][-1].append(game_dict)
+    else:
+        history['moves'].append([game_dict])
+
 ## common new game functions
 def display_move(user, move, user_name = None):
-    prefix_space = user == "user" 
+    prefix_space = user == "user"
     msg = f'{RPS_COMMENTS[f'{user}_move']}{move}'
     if user_name:
         msg = msg.replace('user', user_name)
     prompt(msg, prefix_space = prefix_space)
 
+## Create Game History functions
+def save_game_history(settings, history):
+    file_name = settings['game_history_filename']
+    json_file_path = get_game_history_path(file_name)
+    append_history_to_file(json_file_path, history)
+    return json_file_path
+
+def get_game_history_path(file_name):
+    json_file_path = get_json_file_path(file_name)
+
+    if not(os.path.exists(json_file_path)):
+        write_to_json_file(json_file_path, [])
+    return json_file_path
+
+def append_history_to_file(json_file_path, history):
+    prev_history = load_json_file(json_file_path)
+    prev_history.append(history)
+    write_to_json_file(json_file_path, prev_history)
+
+## Create Stats Functions
+def display_user_stats(settings):
+    sort_function, reverse = (None, True)
+    history = load_game_history(settings)
+    while True:
+        give_user_stats_intro()
+        user_perf = get_user_perf(history)
+        user_stats = {user:get_user_stats(result)
+                      for user, result in user_perf.items()}
+        sel_players = get_selected_players(user_stats, sort_function, reverse)
+        pretty_print(user_stats, sel_players)
+        sort_function, reverse = get_table_sort_params(sort_function, reverse)
+        if (sort_function is None) and (reverse is None):
+            break
+    return user_stats
+
+def load_game_history(settings):
+    file_name = f"{settings['game_history_filename']}.json"
+    return load_rps_files(file_name)
+
+def give_user_stats_intro():
+    clear_screen()
+    msg = RPS_COMMENTS["user_stats_intro"]
+    prompt(msg)
+
+def get_user_perf(history):
+    user_performance = {}
+    for match in history:
+        player = match['player_name']
+        winner = match['last_match']
+        user_performance.setdefault(player, [])
+        user_performance[player].append(winner)
+    return user_performance
+
+def get_user_stats(user_perf):
+    return {player:user_perf.count(player) for player in set(user_perf)}
+
+def pretty_print(user_statistics, sel_players):
+    print("\n")
+    print_table_header()
+    for player in sel_players:
+        print_user_result_row(user_statistics, player)
+
+def get_selected_players(user_statistics, sort_fn = None, reverse = True,
+                                                            top_n = 5):
+    match sort_fn:
+        case "w":
+            sort_fn = lambda x: user_statistics[x].get('user', 0)
+        case 'g':
+            sort_fn = lambda x: user_statistics[x].get('user', 0) + \
+                                user_statistics[x].get('computer', 0)
+        case 'p':
+            sort_fn = lambda x: user_statistics[x].get('user', 0) / \
+                                (user_statistics[x].get('computer', 0) +
+                                 user_statistics[x].get('user', 0))
+
+    if sort_fn:
+        sorted_players = sorted(user_statistics, key = sort_fn,
+                                reverse = reverse)
+    else:
+        sorted_players = sorted(user_statistics, reverse = reverse)
+    return sorted_players[:top_n]
+
+def get_table_sort_params(curr_sort_fn, curr_reverse):
+    sort_table_msg = RPS_COMMENTS['sort_table']
+    sort_table_err = RPS_COMMENTS['sort_table_error']
+    valid_input = ['w', 'g', 'p', 'q', 'r']
+    sort_table_user_choice = get_valid_input(sort_table_msg, sort_table_err,
+                                                valid_input)
+    sort_fn, reverse = get_sort_params(sort_table_user_choice,
+                                        curr_sort_fn, curr_reverse)
+    return sort_fn, reverse
+
+def get_sort_params(choice, curr_sort_fn, curr_reverse):
+    match choice:
+        case 'w':
+            return 'w', True
+        case 'p':
+            return 'p', True
+        case 'g':
+            return 'g', True
+        case 'r':
+            return curr_sort_fn, not(curr_reverse)
+        case 'q':
+            return None, None
+
+def print_table_header():
+    line0 = (27*"*-").center(55)[:54]
+    col1 = 'Name'.center(15)
+    col2 = 'Wins'.center(15)
+    col3 = 'Losses'.center(15)
+    line1 = f"| {col1} | {col2} | {col3} |"
+    line2 = (27*"*-").center(55)[:54]
+
+    print(line0)
+    print(line1)
+    print(line2)
+
+def print_user_result_row(all_win_stats, user_name):
+    display_name = user_name[:15].center(15)
+    user_data = all_win_stats[user_name]
+    wins = str(user_data.get('user', 0)).center(15)
+    loss = str(user_data.get('computer', 0)).center(15)
+    line1 = f"| {display_name} | {wins} | {loss} |"
+    line2 = (27*"*-").center(55)[:54]
+    print(line1)
+    print(line2)
+## Check Achievements functions
 #################### quit game functions
 def quit_game():
     clear_screen()
-    prompt(RPS_COMMENTS["exit_game"])
-    time.sleep(2)
+    wave_hands_bye()
+    clear_screen(banner = False)
 
-# high score functions
-def show_high_scores():
-    pass
+def wave_hands_bye():
+    frames = [
+    r"""
+    \o  
+     |  
+    / \ 
+    """,
+    r"""
+     o/ 
+     |  
+    / \ 
+    """
+    ]
+
+    for _ in range(10):
+        for frame in frames:
+            clear_screen()
+            prompt(RPS_COMMENTS["exit_game"])
+            print(frame)
+            time.sleep(0.2)
+
 
 # Pre Load Game Files and Variables
 RPS_SETTINGS = load_rps_files("rps_config.json")
 
 PERSONALITY = RPS_SETTINGS['AI_personality']
-NO_OF_ROUNDS = RPS_SETTINGS["no_of_rounds"]
-PLAYER_NAME = RPS_SETTINGS["name_of_player"].capitalize()
-GAME_MODE = RPS_SETTINGS["game_mode"]
-GAME_HISTORY_PATH = RPS_SETTINGS['game_history_filename']
 MOVE_CHOICE = RPS_SETTINGS['move_choice']
 WINNING_MOVES = RPS_SETTINGS['rules']
 MAIN_SCREEN_OPTIONS = RPS_SETTINGS['main_screen_menu']
@@ -445,8 +626,8 @@ while True:
     match user_introduction_choice:
         case 'help':
             show_help_screen(MOVE_CHOICE, WINNING_MOVES)
-        case 'high_score':
-            show_high_scores()
+        case 'display_stats':
+            display_user_stats(RPS_SETTINGS)
         case 'config':
             updated_config = edit_config(RPS_SETTINGS)
             if updated_config:
@@ -455,8 +636,8 @@ while True:
                 break
         case 'new_game':
             while True:
-                continue_status = start_new_game(RPS_SETTINGS)
-                if not(continue_status):
+                continue_game = start_new_game(RPS_SETTINGS)
+                if not(continue_game):
                     break
         case 'quit_game':
             quit_game()
